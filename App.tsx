a61cc440e3e40a0e29a01b5de22b99dbf23a630a
Dawwie <Dawid Wietrzych>
2024-06-29 10:17:48 +0200

    feat: added fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import { useLoadFonts } from "@/utils/fontConfig";
+import typography from "@/utils/theme/typography";
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const { fontsLoaded, fontError, onLayoutRootView } = useLoadFonts();
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={typography.caption2}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
